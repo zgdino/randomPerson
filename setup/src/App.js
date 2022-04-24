a28a6d036cf811936f94956ba269bb05cad78d6c
Dino Hadzic <zgdino@yahoo.com>
2022-04-24 11:14:16 +0200

    landing page

--- setup/src/App.js
+++ setup/src/App.js
@@ -16,10 +16,74 @@ function App() {
   const [value, setValue] = useState('radnom person')
 
   const handleValue = (e) => {
-    console.log(e.target);
+    console.log(e.target)
   }
 
-  return <h2>random user starter</h2>
+  return (
+    <main>
+      <div className='block bcg-black'></div>
+      <div className='block'>
+        <div className='container'>
+          {/* if the person exists display its image, if not → display default image  */}
+          <img
+            src={(person && person.image) || defaultImage}
+            alt='random user'
+            className='user-img'
+          />
+          <p className='user-title'>my {title} is</p>
+          <p className='user-value'>{value}</p>
+          <div className='values-list'>
+            {/* data label providing value for the state for hovering later */}
+            <button
+              className='icon'
+              data-label='name'
+              onMouseOver={handleValue}
+            >
+              <FaUser />
+            </button>
+            <button
+              className='icon'
+              data-label='email'
+              onMouseOver={handleValue}
+            >
+              <FaEnvelopeOpen />
+            </button>
+            <button
+              className='icon'
+              data-label='age'
+              onMouseOver={handleValue}
+            >
+              <FaCalendarTimes />
+            </button>
+            <button
+              className='icon'
+              data-label='street'
+              onMouseOver={handleValue}
+            >
+              <FaMap />
+            </button>
+            <button
+              className='icon'
+              data-label='phone'
+              onMouseOver={handleValue}
+            >
+              <FaPhone />
+            </button>
+            <button
+              className='icon'
+              data-label='password'
+              onMouseOver={handleValue}
+            >
+              <FaLock />
+            </button>
+          </div>
+          <button className="btn" type='button'>
+            {loading? 'loading...' : 'random user'}
+          </button>
+        </div>
+      </div>
+    </main>
+  )
 }
 
 export default App
