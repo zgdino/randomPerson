17af4e8961212d8fa08717002cbecfc75ee0a0ee
Dino Hadzic <zgdino@yahoo.com>
2022-04-27 21:09:53 -1000

    comments

--- setup/src/index.js
+++ setup/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// importing css file here because it affects App.js
 import './index.css';
 import App from './App';
 
+// rendering App from App.js
 ReactDOM.render(
   <React.StrictMode>
     <App />
